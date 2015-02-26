f7beff0fd316dd4a0f674abcf6e48604a1c10481
Ørjan Frantzen <orjan_f93@hotmail.com>
2015-02-26 11:56:28 +0100

    nada

--- CalendarProject/src/calendarProject/Event.java
+++ CalendarProject/src/calendarProject/Event.java
@@ -2,24 +2,43 @@ package calendarProject;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+//ørjan jobber her fra 11.30
 public class Event {
 
-	int eventID;
+	private int eventID;
 
-	String eventName;
+	private String eventName;
 
-	String eventInfo;
-
-	Date date;
-
-	int timeOfDay;
-
-	Room room;
-
-	List invited = new ArrayList<User>();
+	private String eventInfo;
 	
-	public void getEvent(){
+	private Date date;
+
+	private int timeOfDay;
+
+	private Room room;
+
+	private ArrayList<User> invited;
+	
+	public Event getEvent(){
+		return this;
+	}
+	
+	Set<Date, timeOfDay>(Type arg) {
 		
+	}
+
+	removeInvite(User person) {
+		
+	}
+
+	invite(User person) {
+		
+	}
+
+	public void notify() {
+		int n = invited.size();
+		for (int i = 0; i < n; i++) {
+			invited.get(i).
+		}
 	}
 }
--- CalendarProject/src/calendarProject/User.java
+++ CalendarProject/src/calendarProject/User.java
@@ -1,5 +1,13 @@
 package calendarProject;
-
+//ørjan jobber her fra 11.30
 public class User {
 
+	private String email;
+
+	private String name;
+
+	private int phoneNumber;
+
+	private PersonalCalendar personalCal;
+	
 }
