58008daffb7cbd553cc76c91c8af3670f6a21bfd
tobiaspettrem <tobias.pettrem@gmail.com>
2015-03-02 13:41:21 +0100

    Lagt til muligheten for å invitere brukere til event, andre forbedringer

--- CalendarProject/src/calendarProject/Event.java
+++ CalendarProject/src/calendarProject/Event.java
@@ -1,44 +1,88 @@
 package calendarProject;
+
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+
 public class Event {
 
-	private int eventID;
+	private int ID;
 
-	private String eventName;
+	private String name;
 
-	private String eventInfo;
-	
+	private String info;
+
 	private Date date;
-
-	private int timeOfDay;
 
 	private Room room;
 
 	private ArrayList<User> invited;
-	
-	public Event getEvent(){
+
+	public Event(String name, Date date) {
+		this.setName(name);
+		this.setDate(date);
+	}
+
+	public Event getEvent() {
 		return this;
 	}
-	
-	Set<Date, timeOfDay>(Type arg) {
-		
+
+	public void notify(int userID, Event event) {
+		// denne metoden må endres totalt sannsynligvis
+		/*
+		 * int n = invited.size(); for (int i = 0; i < n; i++) { invited.get(i);
+		 * }
+		 */
 	}
 
-	removeInvite(User person) {
-		
+	public void inviteUser(int userID) {
+		// finn riktig bruker ved å søke opp i databasen
+		// og legg til personen i lista
+		// deretter må personen blir notified.
 	}
 
-	invite(User person) {
-		
-	}
-
-	public void notify(String string) {
-		//denne metoden må endres totalt sannsynligvis
-		int n = invited.size();
-		for (int i = 0; i < n; i++) {
-			invited.get(i).
+	public void inviteUsers(ArrayList<Integer> userIDs) {
+		for (int userID : userIDs) {
+			inviteUser(userID);
 		}
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
 	}
 }
--- CalendarProject/src/calendarProject/PersonalCalendar.java
+++ CalendarProject/src/calendarProject/PersonalCalendar.java
@@ -3,24 +3,24 @@ package calendarProject;
 import java.util.Date;
 
 public class PersonalCalendar extends Calendar {
-	
+
 	public User owner;
-	
-	public void setOwner(User owner){
+
+	public void setOwner(User owner) {
 		this.owner = owner;
 	}
-	
-	
-	public static void main(String[] args) {
-		final Date date = new Date();
-		System.out.println(date);
+
+	public void addEvent(String name, Date date) {
+		Event event = new Event(name, date);
+		events.add(event);
 	}
-	
-	
-	
-	
-	
-	public void addEvent(String name, Date date){
-		Event event = new Event()
+
+	public void removeEvent(int ID) {
+		for (int i = 0; i < events.size() - 1; i++) {
+			if (events.get(i).getID() == ID) {
+				events.remove(i);
+				i = events.size();
+			}
+		}
 	}
 }
