37ddc72209db1c2d75e7bd25946d458e1deae59e
tobiaspettrem <tobias.pettrem@gmail.com>
2015-03-03 10:06:30 +0100

    IO

--- CalendarProject/src/calendarProject/CalendarIO.java
+++ CalendarProject/src/calendarProject/CalendarIO.java
@@ -1,27 +1,34 @@
 package calendarProject;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
+import sun.util.calendar.Gregorian;
+
 public class CalendarIO {
-	
-	//Denne klassen er i praksis nå brukt som en printer og ikke en komplett IO. Dette er for å teste muligheten for å lagre div. menyer på ett sted.
+
+	// Denne klassen er i praksis nå brukt som en printer og ikke en komplett
+	// IO. Dette er for å teste muligheten for å lagre div. menyer på ett
+	// sted.
 
 	int dybde = 0;
-	
+
 	public void mainMenu() {
+		printCalendar();
 		System.out.println("Dette er hovedmenyen:");
-		System.out.println("1. Tobias elsker pikk.");
+		System.out.println("1. ");
 		System.out.println("2. Ørjan er Sondres sjelefrende.");
 		System.out.println("3. Andrea Marie elsker rapporter.");
 		System.out.println("4. Jarl har nice skjegg.");
 		System.out.print("Enter input: ");
 	}
-	
+
 	public void subMenu(int subMenu) {
 		if (dybde == 0) {
 			switch (subMenu) {
 			case 0:
-				if (dybde > 0) dybde -=1;
+				if (dybde > 0)
+					dybde -= 1;
 				break;
 			case 1:
 				System.out.println("Hvor mye/hva slags pikk elsker Tobias?");
@@ -32,7 +39,7 @@ public class CalendarIO {
 				System.out.println("5. Lange peniser.");
 				dybde += 1;
 				break;
-			case 2: 
+			case 2:
 				System.out.println("Svaret er ja, men hvor ja?");
 				System.out.println("1. Uendelig ja.");
 				System.out.println("2. Særs ja.");
@@ -45,9 +52,25 @@ public class CalendarIO {
 				System.out.println("Korte rapporter.");
 				System.out.println("Tykke rapporter.");
 				System.out.println("Tynne rapporter.");
-				dybde +=1;
+				dybde += 1;
 				break;
 			}
 		}
+	}
+
+	public void printCalendar() {
+		GregorianCalendar cal = new GregorianCalendar();
+		int monthNumber = cal.get(GregorianCalendar.MONTH); // m�nedsnummer
+		int year = cal.get(GregorianCalendar.YEAR); // det aktuelle �ret
+		cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH); // antall dager i m�neden
+		String monthString = new SimpleDateFormat("MMMMMMMMM").format(cal
+				.getTime()); // m�neden som en string
+		System.out.println("              " + monthString);
+		System.out.println("man  tir  ons  tor  fre  l�r  s�n");
+	}
+
+	public static void main(String[] args) {
+		CalendarIO io = new CalendarIO();
+		io.printCalendar();
 	}
 }
