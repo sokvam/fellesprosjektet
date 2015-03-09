0c3d198353e7beafc882390931773472e2847f3d
tobiaspettrem <tobias.pettrem@gmail.com>
2015-03-09 13:44:55 +0100

    Tobias koder på IO

--- CalendarProject/src/calendarProject/CalendarIO.java
+++ CalendarProject/src/calendarProject/CalendarIO.java
@@ -1,4 +1,3 @@
-
 package calendarProject;
 
 import java.text.SimpleDateFormat;
@@ -9,16 +8,26 @@ public class CalendarIO {
 	// Denne klassen er i praksis nå brukt som en printer og ikke en komplett
 	// IO. Dette er for å teste muligheten for å lagre div. menyer på ett
 	// sted.
+	
+	
+	
+	
+	//Tobias koder, stay out! 9. mars 13:45
+	
+	
+	
 
 	int dybde = 0;
 
+	SQLMethods sql = new SQLMethods();
+	Scanner scanner = new Scanner();
+
 	public void mainMenu() {
-		printCalendar();
 		System.out.println("Dette er hovedmenyen:");
-		System.out.println("1. Fiks denne menyen Sondre!");
-		System.out.println("2. Fiks denne menyen Sondre!");
-		System.out.println("3. Fiks denne menyen Sondre!");
-		System.out.println("4. Fiks denne menyen Sondre!");
+		System.out.println("1. Opprett ny bruker");
+		System.out.println("2. Logg inn");
+		System.out.println("3. Kommer snart!");
+		System.out.println("4. Kommer snart!");
 		System.out.print("Enter input: ");
 	}
 
@@ -30,12 +39,7 @@ public class CalendarIO {
 					dybde -= 1;
 				break;
 			case 1:
-				System.out.println("Hvor mye/hva slags pikk elsker Tobias?");
-				System.out.println("1. All pikk.");
-				System.out.println("2. Kun hvit.");
-				System.out.println("3. Kun varm.");
-				System.out.println("4. Korte peniser.");
-				System.out.println("5. Lange peniser.");
+				
 				dybde += 1;
 				break;
 			case 2:
@@ -60,45 +64,47 @@ public class CalendarIO {
 	public void printCalendar() {
 		GregorianCalendar cal = new GregorianCalendar();
 		int year = cal.get(GregorianCalendar.YEAR);
-		int daysInMonth = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH); // antall dager i måneden
-		String monthString = new SimpleDateFormat("MMMMMMMMM").format(cal.getTime()); // måneden som en string
-		int dayNumberInMonth = cal.get(GregorianCalendar.DAY_OF_MONTH); //1-indeksert
-		int dayNumberInWeek = (cal.get(GregorianCalendar.DAY_OF_WEEK) + 6) % 7; //0-indeksert, starter mandag
-		int firstDayOfMonth = ((7 - (dayNumberInMonth % 7)) + dayNumberInWeek) % 7;//første ukedagen i måneden,
-		//nullindeksert (0 er mandag, 1 er tirsdag, ..., 6 er søndag
+		int daysInMonth = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH); // antall
+																				// dager
+																				// i
+																				// måneden
+		String monthString = new SimpleDateFormat("MMMMMMMMM").format(cal
+				.getTime()); // måneden som en string
+		int dayNumberInMonth = cal.get(GregorianCalendar.DAY_OF_MONTH); // 1-indeksert
+		int dayNumberInWeek = (cal.get(GregorianCalendar.DAY_OF_WEEK) + 6) % 7; // 0-indeksert,
+																				// starter
+																				// mandag
+		int firstDayOfMonth = ((7 - (dayNumberInMonth % 7)) + dayNumberInWeek) % 7;// første
+																					// ukedagen
+																					// i
+																					// måneden,
+		// nullindeksert (0 er mandag, 1 er tirsdag, ..., 6 er søndag
 		System.out.println();
-		System.out.println("\t\t\t" + monthString + " " +  year);
+		System.out.println("\t\t\t" + monthString + " " + year);
 		System.out.println();
 		System.out.println("\tman\ttir\tons\ttor\tfre\tlør\tsøn");
 		int dayCount = 1;
 		int i = 0;
 		int currentWeekday = firstDayOfMonth;
 		boolean run = true;
-		while(run){
-			if(i < firstDayOfMonth){  //Hvis ikke måneden har begynt enda, print tomme felter
+		while (run) {
+			if (i < firstDayOfMonth) { // Hvis ikke måneden har begynt enda,
+										// print tomme felter
 				System.out.print("\t");
 				i++;
-			} else if (dayCount <= daysInMonth){
+			} else if (dayCount <= daysInMonth) {
 				System.out.print("\t" + dayCount);
-				if(dayCount % 32 == 0){ //						FYLL INN: Hvis dagen har en avtale
+				if (dayCount % 32 == 0) { // FYLL INN: Hvis dagen har en avtale
 					System.out.print("*");
 				}
 				dayCount++;
-				if(currentWeekday == 6){ //Hvis søndag: Linjeskift
+				if (currentWeekday == 6) { // Hvis søndag: Linjeskift
 					System.out.println("");
 				}
-				currentWeekday = (currentWeekday + 1) % 7; //Oppdater ukedag
+				currentWeekday = (currentWeekday + 1) % 7; // Oppdater ukedag
 			} else {
 				run = false;
 			}
 		}
-		
-	}
-
-	public static void main(String[] args) {
-		GregorianCalendar cal = new GregorianCalendar();
-		//System.out.println(cal.get(GregorianCalendar.DAY_OF_MONTH));
-		CalendarIO io = new CalendarIO();
-		io.printCalendar();
 	}
 }
