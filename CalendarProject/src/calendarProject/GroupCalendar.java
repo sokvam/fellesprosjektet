03e8b5e8a38ec32e6c9eec9c3043dfc5772daf5a
tobiaspettrem <tobias.pettrem@gmail.com>
2015-03-09 13:28:14 +0100

    _

--- CalendarProject/src/calendarProject/GroupCalendar.java
+++ CalendarProject/src/calendarProject/GroupCalendar.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 public class GroupCalendar extends Calendar{
 	
 	ArrayList<User> members;
-	
+	/*
 	public void addMember(User user, int count){
 		//Forklaring av count ligger i User-klassen.
 		//Enkelt sagt er den til for å hindre at funksjonene kaller hverandre i det uendelige.
 		if(count < 2 && count >= 0){
 			members.add(user);
-			user.addGroupCalendar(this, count + 1);
+			user.addGroupCalendar(this);
 		}
-	}
+	}*/
+	
+	//Trenger denne metoden? Usikker!
 }
--- CalendarProject/src/calendarProject/PersonalCalendar.java
+++ CalendarProject/src/calendarProject/PersonalCalendar.java
@@ -3,14 +3,15 @@ package calendarProject;
 public class PersonalCalendar extends Calendar {
 
 	private User owner;
-
+	
+	/*
 	public void setOwner(User owner, int count) {
 		if (count < 2 && count >= 0) {
 			this.owner = owner;
 			owner.setPersonalCalendar(this, count + 1);
 		}
 	}
-
+	*/
 	public User getOwner() {
 		return this.owner;
 	}
