fed0121bae5169d0c763e7d9132b6a47345adb1f
amfestoy <andreamarie@festoy.no>
2015-03-17 11:47:56 +0100

    io stuff

--- CalendarProject/src/calendarProject/CalendarIO.java
+++ CalendarProject/src/calendarProject/CalendarIO.java
@@ -15,10 +15,10 @@ public class CalendarIO {
 	int userID;
 
 	SQLMethods sql = new SQLMethods();
-	
+
 	public void logIn() {
 
-		System.out.print( "Skriv inn email: ");
+		System.out.print("Skriv inn email: ");
 		String email = scanner.next();
 
 		System.out.print("Skriv inn passord: ");
@@ -28,7 +28,8 @@ public class CalendarIO {
 			userID = sql.getUserID(email);
 			mainMenu();
 		} else {
-			System.out.println("Brukernavn og passord stemte ikke, prøv på nytt");
+			System.out
+					.println("Brukernavn og passord stemte ikke, prøv på nytt");
 			logIn();
 		}
 	}
@@ -37,12 +38,12 @@ public class CalendarIO {
 		System.out.println("1. Opprett ny bruker");
 		System.out.println("2. Logg inn");
 		System.out.print("Enter input: ");
-		scanner.nextLine();
 		int choice = -1;
-		try{
+		try {
 			choice = scanner.nextInt();
-		} catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("Skjønte ikke input.");
+			scanner.nextLine();
 			start();
 		}
 		if (choice == 1) {
@@ -89,9 +90,17 @@ public class CalendarIO {
 			System.out.println(datestring);
 			dayMenu(datestring);
 		case 2:
-			System.out.print("Skriv inn dato på formen 'YYYY-MM-DD': ");
-			String inputdate = scanner.next();
-			dayMenu(inputdate);
+			String inputDate;
+			while(true){
+				System.out.print("Skriv inn dato på formen 'YYYY-MM-DD': ");
+				inputDate = scanner.next();
+				if (isValidDate(inputdate)) {
+					break;
+				} else {
+					System.out.println("Ugyldig dato.");
+				}
+			}
+			dayMenu(inputDate);				
 		case 3:
 			GregorianCalendar greg = new GregorianCalendar();
 			System.out.print("Måned: ");
@@ -107,8 +116,9 @@ public class CalendarIO {
 	}
 
 	public void dayMenu(String date) {
-		for(int i : sql.getEventsForDate(date, userID)){
-			System.out.println("ID: " + i + " | Event: " + sql.getEventInfo(i).getName());
+		for (int i : sql.getEventsForDate(date, userID)) {
+			System.out.println("ID: " + i + " | Event: "
+					+ sql.getEventInfo(i).getName());
 		}
 		System.out.println("1. Velg event");
 		System.out.println("2. Opprett event");
@@ -123,8 +133,8 @@ public class CalendarIO {
 			createEvent(date);
 		case 123:
 			mainMenu();
-		default :
-			System.out.println("Skriv et gyldig valg.");	
+		default:
+			System.out.println("Skriv et gyldig valg.");
 			dayMenu(date);
 		}
 	}
@@ -151,13 +161,13 @@ public class CalendarIO {
 			dayMenu(date);
 		case 123:
 			mainMenu();
-		default :
+		default:
 			System.out.println("Skriv et gyldig valg.");
 			showEvent(eventID, date);
 		}
 	}
-	
-	public void editEvent(int eventID){
+
+	public void editEvent(int eventID) {
 		System.out.println("1. Endre navn");
 		System.out.println("2. Endre starttid");
 		System.out.println("3. Endre slutttid");
@@ -165,7 +175,7 @@ public class CalendarIO {
 		System.out.println("5. Endre Info");
 		System.out.println("123 for å gå til hovedmenyen.");
 		int input = scanner.nextInt();
-		switch(input) {
+		switch (input) {
 		case 1:
 			System.out.print("Skriv inn nytt navn: ");
 			String newname = scanner.next();
@@ -184,19 +194,20 @@ public class CalendarIO {
 		case 4:
 			System.out.println("1. Legg til deltakere:");
 			System.out.println("2. Fjern deltakere");
-			try{
+			try {
 				input = scanner.nextInt();
-			} catch(Exception e){
+			} catch (Exception e) {
 				System.out.println("Skjønte ikke input.");
 				mainMenu();
 			}
 			String inputemail = "";
 			String participants = "";
-			switch(input) {
+			switch (input) {
 			case 1:
-				System.out.println("Skriv inn email til deltakeren, skriv ferdig når du er ferdig: ");
+				System.out
+						.println("Skriv inn email til deltakeren, skriv ferdig når du er ferdig: ");
 				inputemail = scanner.next();
-				while(inputemail != "ferdig") {
+				while (inputemail != "ferdig") {
 					participants = participants + inputemail + ":";
 					inputemail = scanner.next();
 				}
@@ -205,20 +216,21 @@ public class CalendarIO {
 			case 2:
 				Event event = sql.getEventInfo(eventID);
 				System.out.println(event.getInvitedString());
-				System.out.println("Skriv inn mailen på de du ønsker å slette, skriv ferdig når du er ferdig: ");
+				System.out
+						.println("Skriv inn mailen på de du ønsker å slette, skriv ferdig når du er ferdig: ");
 				inputemail = scanner.next();
-				while(inputemail != "ferdig") {
+				while (inputemail != "ferdig") {
 					participants = participants + inputemail + ":";
 					inputemail = scanner.next();
 				}
 				sql.updateEvent(eventID, 42, participants);
 				editEvent(eventID);
-			
+
 			case 123:
 				mainMenu();
 			default:
 				System.out.println("Skriv et gyldig valg.");
-				
+
 			}
 		case 5:
 			System.out.println(sql.getEventInfo(eventID).getDescription());
@@ -226,10 +238,10 @@ public class CalendarIO {
 			String newinfo = scanner.next();
 			sql.updateEvent(eventID, 5, newinfo);
 			editEvent(eventID);
-		
+
 		case 123:
 			mainMenu();
-		default :
+		default:
 			System.out.println("Skriv et gyldig valg.");
 			editEvent(eventID);
 		}
@@ -271,31 +283,31 @@ public class CalendarIO {
 		int hour = date.get(Calendar.HOUR_OF_DAY);
 		int min = date.get(Calendar.MINUTE);
 		int sec = date.get(Calendar.SECOND);
-		
+
 		String stringDay;
 		String stringMonth;
 		String stringHour;
-		
-		if(day < 10){
+
+		if (day < 10) {
 			stringDay = "0" + day;
 		} else {
 			stringDay = "" + day;
 		}
-		
-		if(month < 10){
+
+		if (month < 10) {
 			stringMonth = "0" + month;
 		} else {
 			stringMonth = "" + month;
 		}
-		
-		if(hour < 10){
+
+		if (hour < 10) {
 			stringHour = "0" + hour;
 		} else {
 			stringHour = "" + hour;
 		}
-		
-		SQLString = "'" + year + "-" + stringMonth + "-" + stringDay + " " + hour + ":"
-				+ min + ":" + sec + "'";
+
+		SQLString = "'" + year + "-" + stringMonth + "-" + stringDay + " "
+				+ hour + ":" + min + ":" + sec + "'";
 
 		return SQLString;
 	}
@@ -304,11 +316,12 @@ public class CalendarIO {
 		scanner.nextLine();
 		System.out.print("Skriv inn navn på eventen: ");
 		String name = scanner.nextLine();
-		System.out.print("Skriv inn tidspunkt for eventen på formen yyyy-mm-dd hh:mm:ss: ");
+		System.out
+				.print("Skriv inn tidspunkt for eventen på formen yyyy-mm-dd hh:mm:ss: ");
 		String starttime = "'" + scanner.nextLine() + "'";
 		System.out.print("Skriv inn sluttid: ");
 		String endtime = "'" + scanner.nextLine() + "'";
-		System.out.print("Skriv inn informasjon om eventen, maks 150tegn: "); 
+		System.out.print("Skriv inn informasjon om eventen, maks 150tegn: ");
 		String info = scanner.nextLine();
 		System.out.print("Skriv inn minimum romstørelse: ");
 		int size = scanner.nextInt();
@@ -316,7 +329,8 @@ public class CalendarIO {
 		String answer = scanner.next().toLowerCase();
 		ArrayList<Integer> invites = new ArrayList<Integer>();
 		if (answer == "ja") {
-			System.out.println("Skriv inn email til brukere skriv ferdig når du ikke vil invitere fler: ");
+			System.out
+					.println("Skriv inn email til brukere skriv ferdig når du ikke vil invitere fler: ");
 			String inputemail = scanner.next();
 			while (inputemail != "ferdig") {
 				int inviteID = sql.getUserID(inputemail);
@@ -328,88 +342,75 @@ public class CalendarIO {
 		System.out.println("Eventet er nå opprettet");
 		dayMenu(date);
 	}
-		
 
 	public void printMonth(GregorianCalendar cal) {
-		
+
 		int currentYear = cal.get(Calendar.YEAR);
 		int currentMonth = cal.get(Calendar.MONTH);
-		String monthString = new SimpleDateFormat("MMMMMMMMM").format(cal.getTime()); // måneden som en string
+		String monthString = new SimpleDateFormat("MMMMMMMMM").format(cal
+				.getTime()); // måneden som en string
 		int i = 0;
-		
+
 		System.out.println();
 		System.out.println("\t\t\t" + monthString + " " + currentYear);
 		System.out.println();
 		System.out.println("\tman\ttir\tons\ttor\tfre\tlør\tsøn");
-				
+
 		boolean monthHasStarted = false;
 
-		while (cal.get(Calendar.MONTH) == currentMonth){ //så lenge det ikke har blitt neste måned
-			if(i < ((cal.get(GregorianCalendar.DAY_OF_WEEK) + 5) % 7) && !monthHasStarted){
+		while (cal.get(Calendar.MONTH) == currentMonth) { // så lenge det ikke
+															// har blitt neste
+															// måned
+			if (i < ((cal.get(GregorianCalendar.DAY_OF_WEEK) + 5) % 7)
+					&& !monthHasStarted) {
 				System.out.print("\t");
 				i++;
 			} else {
 				monthHasStarted = true;
 				System.out.print("\t" + cal.get(Calendar.DATE));
-				
-				if(sql.getEventsForDate(convertDateToSQL(cal), userID).size() > 0){
+
+				if (sql.getEventsForDate(convertDateToSQL(cal), userID).size() > 0) {
 					System.out.print("*");
 				}
-				
-				if (cal.get(Calendar.DAY_OF_WEEK) == 1) { // Hvis søndag: Linjeskift
+
+				if (cal.get(Calendar.DAY_OF_WEEK) == 1) { // Hvis søndag:
+															// Linjeskift
 					System.out.println("");
 				}
-				cal.set(currentYear,currentMonth, cal.get(Calendar.DATE) + 1); //øk dato med én
+				cal.set(currentYear, currentMonth, cal.get(Calendar.DATE) + 1); // øk
+																				// dato
+																				// med
+																				// én
 			}
-		} System.out.println("");
+		}
+		System.out.println("");
 	}
-		
-		//________________________
-		//gammel kode: Input kan være hvilken som helst dag i måneden.
-		/*
 
-		int year = cal.get(Calendar.YEAR);
-
-		int daysInMonth = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH); // antall
-																				// dager
-																				// i
-																				// måneden
-		String monthString = new SimpleDateFormat("MMMMMMMMM").format(cal
-				.getTime()); // måneden som en string
-		int dayNumberInMonth = cal.get(GregorianCalendar.DAY_OF_MONTH); // 1-indeksert
-		int dayNumberInWeek = (cal.get(GregorianCalendar.DAY_OF_WEEK) + 6) % 7; // 0-indeksert,
-																				// starter
-																				// mandag
-		int firstDayOfMonth = ((7 - (dayNumberInMonth % 7)) + dayNumberInWeek) % 7;// første
-																					// ukedagen
-																					// i
-																					// måneden,
-		// nullindeksert (0 er mandag, 1 er tirsdag, ..., 6 er søndag
-		System.out.println();
-		System.out.println("\t\t\t" + monthString + " " + year);
-		System.out.println();
-		System.out.println("\tman\ttir\tons\ttor\tfre\tlør\tsøn");
-		int dayCount = 1;
-		int i = 0;
-		int currentWeekday = firstDayOfMonth;
-		boolean run = true;
-		while (run) {
-			if (i < firstDayOfMonth) { // Hvis ikke måneden har begynt enda,
-										// print tomme felter
-				System.out.print("\t");
-				i++;
-			} else if (dayCount <= daysInMonth) {
-				System.out.print("\t" + dayCount);
-				if (dayCount % 32 == 0) { // FYLL INN: Hvis dagen har en avtale
-					System.out.print("*");
-				}
-				dayCount++;
-				if (currentWeekday == 6) { // Hvis søndag: Linjeskift
-					System.out.println("");
-				}
-				currentWeekday = (currentWeekday + 1) % 7; // Oppdater ukedag
-			} else {
-				run = false;
-			}
-		} System.out.println(""); */
+	// ________________________
+	// gammel kode: Input kan være hvilken som helst dag i måneden.
+	/*
+	 * 
+	 * int year = cal.get(Calendar.YEAR);
+	 * 
+	 * int daysInMonth = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+	 * // antall // dager // i // måneden String monthString = new
+	 * SimpleDateFormat("MMMMMMMMM").format(cal .getTime()); // måneden som en
+	 * string int dayNumberInMonth = cal.get(GregorianCalendar.DAY_OF_MONTH); //
+	 * 1-indeksert int dayNumberInWeek = (cal.get(GregorianCalendar.DAY_OF_WEEK)
+	 * + 6) % 7; // 0-indeksert, // starter // mandag int firstDayOfMonth = ((7
+	 * - (dayNumberInMonth % 7)) + dayNumberInWeek) % 7;// første // ukedagen //
+	 * i // måneden, // nullindeksert (0 er mandag, 1 er tirsdag, ..., 6 er
+	 * søndag System.out.println(); System.out.println("\t\t\t" + monthString +
+	 * " " + year); System.out.println();
+	 * System.out.println("\tman\ttir\tons\ttor\tfre\tlør\tsøn"); int dayCount =
+	 * 1; int i = 0; int currentWeekday = firstDayOfMonth; boolean run = true;
+	 * while (run) { if (i < firstDayOfMonth) { // Hvis ikke måneden har begynt
+	 * enda, // print tomme felter System.out.print("\t"); i++; } else if
+	 * (dayCount <= daysInMonth) { System.out.print("\t" + dayCount); if
+	 * (dayCount % 32 == 0) { // FYLL INN: Hvis dagen har en avtale
+	 * System.out.print("*"); } dayCount++; if (currentWeekday == 6) { // Hvis
+	 * søndag: Linjeskift System.out.println(""); } currentWeekday =
+	 * (currentWeekday + 1) % 7; // Oppdater ukedag } else { run = false; } }
+	 * System.out.println("");
+	 */
 }
